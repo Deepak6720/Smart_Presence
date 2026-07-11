e49922bcd243abc17b13d365e65f5a66d99bbeff
Deepak Kumar <ananddeepak97941@gmail.com>
2026-07-12 05:18:49 +0530

    fixed vite configuration

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,28 +7,6 @@ export default defineConfig({
     exclude: ['@vladmandic/face-api']
   },
   build: {
-    chunkSizeWarningLimit: 6000,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('@vladmandic/face-api')) {
-            return 'face-api'
-          }
-
-          if (id.includes('recharts')) {
-            return 'recharts'
-          }
-
-          if (
-            id.includes('react') ||
-            id.includes('react-dom') ||
-            id.includes('react-router-dom') ||
-            id.includes('axios')
-          ) {
-            return 'vendor'
-          }
-        }
-      }
-    }
+    chunkSizeWarningLimit: 6000
   }
 })
